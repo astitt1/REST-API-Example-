05c4e5517b93647b1fc1861dc95d93c434c8b1a1
astitt1 <astitt@perscholas.org>
2024-12-11 15:29:45 -0500

    users route

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users = require('./data/users')
+const posts = require('./data/posts')
+
+
+app.get('/', (req, res)=>{
+    res.send('Work in progress!')
+})
+
+//get all users 
+app.get('/api/users', (req, res)=>{
+    res.json(users)
+})
+
+
+
+
+
 
 
 app.listen(port, ()=>{
