27fa40277473c224e6ebb4c6f009ce3190abb8b6
astitt1 <astitt@perscholas.org>
2024-12-12 11:53:11 -0500

    don't use bpdyparsergit add .git add . router.use(express.json())

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = 3000;
 const users = require("./data/users");
 const posts = require("./data/posts");
 
+const userRoutes = require('./routes/userRoutes')
+
 const bodyParser = require("body-parser");
 
+//MIDDLEWARE
+app.use('/api/users', userRoutes)
+// app.use('/api/posts', postRoutes)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
@@ -14,60 +19,6 @@ app.get("/", (req, res) => {
   res.send("Work in progress!");
 });
 
-//get all users
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
-
-app.post("/api/users", (req, res) => {
-  if (req.body.name && req.body.username && req.body.email) {
-    if (users.find((u) => u.username == req.body.username)) {
-      res.json({ error: "Username Already Taken" });
-      return;
-    }
-    //move code here
-    const user = {
-      id: users[users.length - 1].id + 1,
-      name: req.body.name,
-      username: req.body.username,
-      email: req.body.email,
-    };
-
-    users.push(user);
-    res.json(users[users.length - 1]);
-  } else res.json({ error: "Insufficent Data" });
-});
-
-app.get("/api/users/:id", (req, res, next) => {
-  const user = users.find((u) => u.id == req.params.id);
-  if (user) res.json(user);
-  else next();
-});
-
-app.patch("/api/users/:id", (req, res) => {
-  const user = users.find((u, i) => {
-    if (u.id == req.params.id) {
-      for (const key in req.body) {
-        users[i][key] = req.body[key];
-      }
-    }
-    return true;
-  });
-
-  if (user) res.json(user);
-  else next();
-});
-
-app.delete("/api/users/:id", (req, res) => {
-  const user = users.find((u, i) => {
-    if (u.id == req.params.id) {
-      users.splice(i, 1);
-      return true;
-    }
-  });
-
-  if (user) res.json(user);
-});
 
 //posts
 app.get("/api/posts", (req, res) => {
